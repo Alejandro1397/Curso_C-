fcfb3006194fee4813a4405d2bf7ebea3dbeb56d
Alejandro1397 <alejandro.posadasjr@gmail.com>
2020-02-10 22:28:32 -0600

    Ejercicio 1

---
+++ Serie_1/Ejercico_1.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Serie_1{
+    class Suma{
+        static void Main(string[] args){
+            //Se le indica a el usuario que indique el limite de numeros a sumae
+            Console.WriteLine("Ingrese el limite de numeros a sumar");
+            int n=0;
+            try
+            {
+                //Leemos la entrada del usuario y luego la convertimos a entero
+                n = Convert.ToInt32(Console.ReadLine());
+            }catch(FormatException)
+            {
+                Console.WriteLine("Solo se aceptan enteros");
+            }
+            //Creamos una variable donde almacenaremos el resultado de la suma
+            int suma = 0;
+            //Creamos un ciclo for con el cual realizaremos la suma desde uno hasta
+            //el limite que nos indico el usuario
+            for (int i = 1; i <= N; i++){
+                //se agrega el valor de cada repeticion a la variable suma
+                suma += i;
+            }
+            //Se imprime el resutado de la suma
+            Console.WriteLine("El resultado es: " + suma);
+            Console.ReadKey();
+        }
+    }
+}
