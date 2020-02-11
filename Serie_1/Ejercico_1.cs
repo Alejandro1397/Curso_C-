de69072058023fb878948d56bb897e7dc8c5487b
Alejandro1397 <alejandro.posadasjr@gmail.com>
2020-02-10 22:39:56 -0600

    Update Ejercico_1.cs

--- Serie_1/Ejercico_1.cs
+++ Serie_1/Ejercico_1.cs
@@ -12,15 +12,15 @@ namespace Serie_1{
                 n = Convert.ToInt32(Console.ReadLine());
             }catch(FormatException)
             {
-                Console.WriteLine("Solo se aceptan enteros");
+                Console.WriteLine("Solo se aceptan numeros enteros");
             }
             //Creamos una variable donde almacenaremos el resultado de la suma
-            int suma = 0;
+            int res = 0;
             //Creamos un ciclo for con el cual realizaremos la suma desde uno hasta
             //el limite que nos indico el usuario
             for (int i = 1; i <= n; i++){
                 //se agrega el valor de cada repeticion a la variable suma
-                suma += i;
+                res += i;
             }
             //Se imprime el resutado de la suma
             Console.WriteLine("El resultado es: " + res);
