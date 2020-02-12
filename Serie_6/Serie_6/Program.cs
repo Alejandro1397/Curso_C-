89d6d48d116fbd468b78d7ac75ce6cd4fd2336eb
Alejandro1397 <alejandro.posadasjr@gmail.com>
2020-02-11 19:15:36 -0600

    Ejercicio 6

--- Serie_6/Serie_6/Program.cs
+++ Serie_6/Serie_6/Program.cs
@@ -4,13 +4,13 @@ namespace Serie_6{
     class Program{
         static void Main(string[] args){
             //Declaramos dos nuevos objetos
-            CuentaBancaria c1 = new CuentaBancaria("Daniel", 1532.0);
-            CuentaBancaria c2 = new CuentaBancaria("Juan", 783.25);
+            CuentaBancaria c1 = new CuentaBancaria("Yamid", 1750.60);
+            CuentaBancaria c2 = new CuentaBancaria("Rashid", 883.25);
             //Utilizamos los metodos de la clase CuentaBancaria
             c1.mostarInformacion();
-            c1.retirar(500.30);
+            c1.retirar(250.50);
             c2.mostarInformacion();
-            c2.depositar(250.50);
+            c2.depositar(150.50);
             c2.retirar(2000);
 
             Console.ReadKey();
