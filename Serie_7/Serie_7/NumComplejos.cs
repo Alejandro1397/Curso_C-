b0288bda232f8a6feb948f06470a5c5191b0969b
Alejandro1397 <alejandro.posadasjr@gmail.com>
2020-02-11 21:25:35 -0600

    Ejercicio 7

---
+++ Serie_7/Serie_7/NumComplejos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serie_7{
+    class NumComplejos{
+        //Declaramos los atributos necesarios para la clase
+        public int Real;
+        public int Imaginario;
+        //Creamos el constructor que solicita la parte real e imaginaria para crear el número
+        public NumComplejos(int real, int imaginario){
+            Real = real;
+            Imaginario = imaginario;
+        }
+        //Funcion que imprime el numero imaginario
+        public void mostrar(){
+            //Verifica si la parte imaginaria es postivica para imprimirlo con un signo de mas despues de la parte real
+            if (Imaginario > 0){
+                Console.WriteLine("{0}+{1}i", Real, Imaginario);
+                //Si lo anterior no se cumple verifica si la parte imaginaria es 0 para solo imprimir la parte real
+            }else if (Imaginario == 0){
+                Console.WriteLine("{0}", Real);
+                //Si la parte imaginaria es negativa solo imprime la real segida de la imaginaria ya que trae su signo
+            }else{
+                Console.WriteLine("{0}{1}i", Real, Imaginario);
+            }
+        }
+        //Funcion que suma dos numeros complejos
+        public NumComplejos sumar(NumComplejos n1){
+            NumComplejos sum = new NumComplejos(Real + n1.Real, Imaginario + n1.Imaginario);
+            return sum;
+        }
+    }
+}
---
+++ Serie_7/Serie_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Serie_7{
+    class Program{
+        static void Main(string[] args){
+            //Se declaran los objetos
+            NumComplejos n1 = new NumComplejos(10, -1);
+            NumComplejos n2 = new NumComplejos(13, 15);
+            NumComplejos n3;
+            //Se hace uso de los metodos declarados
+            n1.mostrar();
+            n3 = n2.sumar(n1);
+            n3.mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
