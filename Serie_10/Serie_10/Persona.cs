bdf99e401bdc544bf17cea617e1f37c5586eea4a
Alejandro1397 <alejandro.posadasjr@gmail.com>
2020-02-12 21:42:42 -0600

    Ejercicio 10

---
+++ Serie_10/Serie_10/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serie_10{
+    class Persona{
+         // Declaramos los atributos
+        public string Nombre;
+        public int Estatura; //En cm
+        public int Peso; // En kg.
+
+        //Declaramos el constructor
+        public Persona(string nombre, int estatura, int peso){
+            Nombre = nombre;
+            Estatura = estatura;
+            Peso = peso;
+        }
+
+        //Declaramos los métodos
+        public void cmToft(int estatura){
+            double pies = estatura * 0.032808; 
+            Console.WriteLine("\n{0} mide {1} ft. ({2} cm)", Nombre, pies, estatura);
+        }
+
+        public void kgTolb(int peso){
+            double libras = peso * 2.20462;
+            Console.WriteLine("\n{0} pesa {1} libras. ({2} kg)", Nombre, libras, peso);
+        } 
+    }
+}
---
+++ Serie_10/Serie_10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Serie_10{
+    class Program{
+        static void Main(string[] args){
+            // Variables para la ejecución del programa
+            string nombre_ingresado;
+            int altura_ingresada;
+            int peso_ingresado;
+
+            Console.Write("Conversion de peso y altura al sistema inglés.");
+            Console.WriteLine("Ingrese el nombre : ");
+            nombre_ingresado = Console.ReadLine();
+           
+            do{
+                try
+                {
+                    Console.WriteLine("Ingrese la altura en cm : ");
+                    altura_ingresada = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ingrese el dato correspondiente");
+                }
+            } while (true);
+
+
+            do{
+                try
+                {
+                    Console.WriteLine("Peso (kg) : ");
+                    peso_ingresado = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ingrese el dato correspondiente");
+                }
+            } while (true);
+
+            // Creamos el objeto
+            Persona p1 = new Persona(nombre_ingresado, altura_ingresada, peso_ingresado);
+
+            // Se mandan a llamar a los métodos
+            p1.cmToft(altura_ingresada);
+            p1.kgTolb(peso_ingresado);
+
+            Console.ReadKey();
+        }
+    }
+}
